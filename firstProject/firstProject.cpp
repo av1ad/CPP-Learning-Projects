3ebe8895e92ceeb3de3cb5544f05d115d1a74757
av1ad <aviadchuraman@gmail.com>
2026-03-08 19:33:23 -0700

    Integers and doubles

--- firstProject/firstProject.cpp
+++ firstProject/firstProject.cpp
@@ -3,5 +3,17 @@
 int main() {
     std::cout << "I love Dajana" << std::endl;
     std::cout << " - from your husband" << std::endl;
+
+    // Integer (whole numbers only)
+    int age = 22;
+    int year = 2026;
+
+    // Double (numbers with decimals)
+    double heightInCM = 172.72;
+    double priceOfYourFitMonthly = 7.99;
+
+    std::cout << priceOfYourFitMonthly << std::endl;
+
+
     return 0;
 } 
