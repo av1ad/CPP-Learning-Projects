2657555bf4a575a9bc94512397955b6170fbaf5f
av1ad <aviadchuraman@gmail.com>
2026-03-09 17:48:50 -0700

    Consts

---
+++ const/consts.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+
+    // When making a const in cpp - best practice is to use all uppercase and/or snake case for naming conventions
+
+    const double PI = 3.14159;
+    double radius = 7;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << "cm";
+
+
+    const int BIRTH_YEAR = 2004
+
+    return 0;
+}
