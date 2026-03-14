89ff0ec16ad4428a075f2e5221055d01ee2ab47b
av1ad <aviadchuraman@gmail.com>
2026-03-14 14:19:25 -0700

    Using keyword vs typedef

--- typeDefsAndTypeAliases/typeDefsAndTypeAliases.cpp
+++ typeDefsAndTypeAliases/typeDefsAndTypeAliases.cpp
@@ -3,8 +3,11 @@
 
 // typedef std::vector<std::pair<std::string, int>> pairlist_t;
 
-typedef std::string text_t;
-typedef int number_t;
+// typedef std::string text_t;
+// typedef int number_t;
+
+using text_t = std::string; // Better to use
+using number_t = int; // Better to use
 
 int main()
 {
@@ -12,7 +15,7 @@ int main()
     // typedefs = reserved ketword used to create an additional name (alias) for another data type. - Think of nickname
     //  New identifier for an existing type
     //  This helps with readability and reduces typos
-    // Use when there is a clear benefit only
+    // Use when there is a clear benefit only for either typedef/using
     // Replaced with the 'using' keyword (works better w/templates)
 
     // pairlist_t pairlist;
