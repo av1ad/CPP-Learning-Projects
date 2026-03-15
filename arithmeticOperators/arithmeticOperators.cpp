ec73dc3df9c9b24f8cb3bea7b462b4a38c5a4d67
av1ad <aviadchuraman@gmail.com>
2026-03-15 10:49:44 -0700

    Arithmetic operators (Already knew this but good to have in this repo)

---
+++ arithmeticOperators/arithmeticOperators.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+
+    // Arithmetic operators = returns the results of a speciifc
+    // Arithmetic operation (+ - * /)
+
+    int students = 20;
+
+    // students++;
+
+    students--;
+
+    students += 3;
+
+    students*=3;
+
+    students /= 2;
+
+    std::cout << students;
+
+    return 0;
+}
