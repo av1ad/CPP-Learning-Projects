efd62389e29dbbea7f700ba03f20a70c9f41c9d4
av1ad <aviadchuraman@gmail.com>
2026-03-15 10:55:26 -0700

    Included modulus operator

--- arithmeticOperators/arithmeticOperators.cpp
+++ arithmeticOperators/arithmeticOperators.cpp
@@ -5,24 +5,27 @@ int main()
 
     // Arithmetic operators = returns the results of a speciifc
     // Arithmetic operation (+ - * /)
+    // Modulus ( % )
 
-    double students = 20;
+    int students = 20;
 
-    // int students = 20;
+    // // double students = 20;
 
-    // students++;
+    // // students++;
 
-    students--;
+    // students--;
 
-    students += 3;
+    // students += 3;
 
-    students*=3;
+    // students*=3;
 
-    students /= 5;
+    // students /= 5;
 
-    std::cout << students;
+    int remainder = students % 3;
 
-    // If output is a decimal change to a double instead of an int because int truncates the decimal portion 
+    std::cout << remainder;
+
+    // If output is a decimal change to a double instead of an int because int truncates the decimal portion
 
     return 0;
 }
