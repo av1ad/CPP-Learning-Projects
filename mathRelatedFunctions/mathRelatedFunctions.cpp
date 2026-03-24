cc3b5b89ec5fed70b414dbb2ca7ad9ba47a8bd44
av1ad <aviadchuraman@gmail.com>
2026-03-24 16:23:34 -0700

    More math functions

--- mathRelatedFunctions/mathRelatedFunctions.cpp
+++ mathRelatedFunctions/mathRelatedFunctions.cpp
@@ -4,13 +4,35 @@
 int main()
 {
 
-    int age = 22;
+    double x = 3.99;
+    double y = 4;
+    double z;
 
-    if (age > 24)
-    {
-        std::cout << pow(age, 2);
-    } else {
-        std::cout << ":(";
-    }
+    // z = std::max(x,y); // Outputs 4
+
+    // z = std::min(x, y); // Outputs 3
+
+    // z = sqrt(9);
+
+    // z = abs(-100);
+
+    // z = round(x);
+
+    // z = ceil(x);
+
+    // z = floor(x);
+
+    std::cout << z;
+
+    // int age = 22;
+
+    // if (age > 24)
+    // {
+    //     std::cout << pow(age, 2);
+    // }
+    // else
+    // {
+    //     std::cout << ":(";
+    // }
     return 0;
 }
