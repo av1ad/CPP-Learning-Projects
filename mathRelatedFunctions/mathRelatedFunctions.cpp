21ab1ab77016a0cfcd7ffd6f77a6098bcf3ad884
av1ad <aviadchuraman@gmail.com>
2026-03-24 13:21:28 -0700

    Math related functions

--- mathRelatedFunctions/mathRelatedFunctions.cpp
+++ mathRelatedFunctions/mathRelatedFunctions.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <cmath>
 
 int main()
 {
+
+    int age = 22;
+
+    if (age > 24)
+    {
+        std::cout << pow(age, 2);
+    } else {
+        std::cout << ":(";
+    }
     return 0;
 }
