f84bc2f7d2c0e14f03b3c5588083ac56fe76a5f0
av1ad <aviadchuraman@gmail.com>
2026-03-24 17:04:59 -0700

    Added hypotenuse calculator

---
+++ hypotenuseCalculator/hypotenuseCalculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+
+    double a;
+    double b;
+    double c;
+
+    std::cout << "Enter a value for a:" << std::endl;
+    std::cin >> a;
+
+    std::cout << "Enter a value for b:" << std::endl;
+    std::cin >> b;
+
+    c = hypot(a, b);
+
+    std::cout << c;
+
+    return 0;
+}
